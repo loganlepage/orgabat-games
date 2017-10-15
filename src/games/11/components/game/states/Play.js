81584bde06bf96dc3dce0e5a557d36fbbcb451b3
Lotfi idir <lotfiidir1@gitlab.com>
2017-10-15 10:19:33 +0200

    [fix:jeu11] Add delivery background

--- src/games/11/components/game/states/Play.js
+++ src/games/11/components/game/states/Play.js
@@ -30,7 +30,11 @@ export default class Play extends State {
      */
     create() {
         this.game.controlsEnabled = false;
-        this.game.stage.backgroundColor = '#DADAD5';
+        this.game.stage.backgroundColor = '#FFFFFF';
+
+        this.tileSprite = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, "atlas");
+        this.tileSprite.tileScale.set(this.game.SCALE * 1.5);
+        this.tileSprite.alpha = 0.3;
 
         this.initUI();
         PhaserManager.ready('game', 'play');
