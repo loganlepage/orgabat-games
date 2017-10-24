4dc8583887d8e890b1c2173782e7de95589b3b5d
Lotfi idir <lotfiidir1@gitlab.com>
2017-10-24 18:58:36 +0200

    [fix:jeu3] Resize wastes and reduce scale

--- src/games/3/components/game/config/data.js
+++ src/games/3/components/game/config/data.js
@@ -24,6 +24,8 @@ export default {
             {name: "polystyrene", x: 440, y: 80, prop: {}},
             {name: "verre", x: 110, y: 100, prop: {}},
             {name: "terre_non_polluee", x: 280, y: 280, prop: {}},
+            {name: "amiante", x: 80, y: 280, prop: {}},
+            {name: "amiante_ciment", x: 140, y: 280, prop: {}}
         ],
         actions: [
             {name: "remettre_camion", info: "Remettre dans le camion"},
@@ -58,98 +60,110 @@ export default {
             terre_non_polluee: {
                 title: "Terre non polluée",
                 epi: ["protection_des_mains", "protection_des_yeux", "protection_du_corps"],
-                action: ["dechets_industriels_speciaux"],
+                action: ["dechets_inertes"],
                 description: "Description manquante"
             },
             palettes: {
                 title: "Palettes",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "La palette est un plateau de chargement qui permet de rassembler des emballages et de " +
                 "constituer une unité de chargement. C'est une plateforme de stockage, de manutention et de transport."
             },
             bois: {
                 title: "Bois",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "description manquante"
             },
             bois_traite: {
                 title: "Bois traité",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             brosses_et_chiffons_souilles: {
                 title: "Brosses et chiffons souillés",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             cartouches_dangereuses: {
                 title: "Cartouches dangereuses",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             cartouches_non_dangereuses: {
                 title: "Cartouches non dangereuses",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "description manquante"
             },
             dechets_dangereux: {
                 title: "Déchets dangereux",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             emballages_souilles: {
                 title: "Emballages souillés",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             inertes: {
                 title: "Inertes",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_inertes"],
                 description: "description manquante"
             },
             inertes_platre: {
                 title: "Inertes platre",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_inertes"],
                 description: "description manquante"
             },
             isolants: {
                 title: "Isolants",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "description manquante"
             },
             peinture_dangereuse: {
                 title: "Peinture dangereuse",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
             peinture_non_dangereuse: {
                 title: "Peinture non dangereuse",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "description manquante"
             },
             polystyrene: {
                 title: "Polystyrene",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_industriels_banals"],
                 description: "description manquante"
             },
             verre: {
                 title: "Verre",
                 epi: ["protection_des_mains"],
-                action: ["remettre_camion", "dechets_inertes", "enfouir"],
+                action: ["dechets_inertes"],
+                description: "description manquante"
+            },
+            amiante: {
+                title: "Amiante",
+                epi: ["protection_des_mains"],
+                action: ["dechets_industriels_speciaux"],
+                description: "description manquante"
+            },
+            amiante_ciment: {
+                title: "Amiante",
+                epi: ["protection_des_mains"],
+                action: ["dechets_industriels_speciaux"],
                 description: "description manquante"
             },
         },
