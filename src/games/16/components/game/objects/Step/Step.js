c1f2260b7844f85d6f71d3da42efce792c5dea5f
pa-lem <pa-lem@hotmail.fr>
2017-10-31 15:09:28 +0100

    [fix:jeu16] Suppression zones rouges

--- src/games/16/components/game/objects/Step/Step.js
+++ src/games/16/components/game/objects/Step/Step.js
@@ -59,7 +59,7 @@ export default class Step extends BasicGameObject {
         }
 
         // Shapes to answer
-        let fill = true, // to fill or not
+        let fill = false, // to fill or not
             radius = 75 * this.game.SCALE;
         if (this.stepData.shapes != undefined) {
             this.shapes = [];
--- webpack.config.js
+++ webpack.config.js
@@ -22,13 +22,13 @@ module.exports = {
          // '7': "./src/games/7/app.jsx", // Jeu 7
          // '8': "./src/games/8/app.jsx", // Jeu 8
          // '9': "./src/games/9/app.jsx", // Jeu
-         '10': "./src/games/10/app.jsx", // Jeu 10
+         // '10': "./src/games/10/app.jsx", // Jeu 10
          // '11': "./src/games/11/app.jsx", // Jeu 11
          // '12': "./src/games/12/app.jsx", // Jeu 12
          // '13': "./src/games/13/app.jsx", // Jeu 13
          // '14': "./src/games/14/app.jsx", // Jeu 14
          // '15': "./src/games/15/app.jsx", // Jeu 15
-         // '16': "./src/games/16/app.jsx", // Jeu 16
+         '16': "./src/games/16/app.jsx", // Jeu 16
          // '17': "./src/games/17/app.jsx", // Jeu 17
          // '18': "./src/games/18/app.jsx", // Jeu 18
          // '19': "./src/games/19/app.jsx", // Jeu 19
